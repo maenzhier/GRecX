7bb749c9a7ca6bfb9b09486210fc813e16ec0a6d
Desheng Cai <caidsml@gmail.com>
2021-10-29 23:10:40 +0800

    add code

---
+++ grecx/config.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+import os
+# os.environ["CUDA_VISIBLE_DEVICES"] = "5"
+
+# replace data_base_dir with path of your own data directory
+data_base_dir = os.path.join(os.path.dirname(__file__), "../data")
+dataset = "yelp"
+# dataset = "flickr"
+data_dir = os.path.join(data_base_dir, dataset)
+
+
+train_path = os.path.join(data_dir, "{}.train.rating".format(dataset))
+test_path = os.path.join(data_dir, "{}.test.rating".format(dataset))
+links_path = os.path.join(data_dir, "{}.links".format(dataset))
+cached_data_path = os.path.join(data_dir, "cached.p")
+cached_neg_data_path = os.path.join(data_dir, "cached_neg.p")
+
+num_negs = 1000
+# test_neg_path = os.path.join(data_dir, "{}.test.negative".format(dataset))
+
+embedding_size = 64
+# embedding_size = 256
+
+print(os.listdir(data_dir))
--- grecx/data/load_graph.py
+++ grecx/data/load_graph.py
@@ -5,9 +5,9 @@ from diffnet.data.utils import read_graph
 
 def generate_user_item_graph(order):
 
-    user_user_graph = read_graph("/home/caidesheng/social-rec/code/diffnet/data/tmp/{}_user_user_graph.pkl".format(order))
-    item_item_graph = read_graph("/home/caidesheng/social-rec/code/diffnet/data/tmp/{}_item_item_graph.pkl".format(order))
-    user_user_social_graph = read_graph("/home/caidesheng/social-rec/code/diffnet/data/tmp/{}_user_user_social_graph.pkl".format(order))
+    user_user_graph = read_graph("/home/caidesheng/social-grecx/code/diffnet/data/tmp/{}_user_user_graph.pkl".format(order))
+    item_item_graph = read_graph("/home/caidesheng/social-grecx/code/diffnet/data/tmp/{}_item_item_graph.pkl".format(order))
+    user_user_social_graph = read_graph("/home/caidesheng/social-grecx/code/diffnet/data/tmp/{}_user_user_social_graph.pkl".format(order))
 
     user_user_graph = user_user_graph.maximum(user_user_graph.T)
     item_item_graph = item_item_graph.maximum(item_item_graph.T)
