d2e8cfc775daaa7599288b709c7812a15834db04
hujunxianligong <hujunxianligong@gmail.com>
2021-10-29 23:09:22 +0800

    add pip

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+m2r README.md
+rm -rf grecx.egg-info
+rm -rf dist
+python setup.py sdist
+twine upload dist/* --verbose
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grecx",
+    python_requires='>3.5.0',
+    version="0.0.1",
+    author="Desheng Cai",
+    author_email="caidsml@gmail.com",
+    packages=find_packages(
+        exclude=[
+            'benchmarks',
+            'data',
+            'demo',
+            'dist',
+            'doc',
+            'docs',
+            'logs',
+            'models',
+            'test'
+        ]
+    ),
+    install_requires=[
+        "tf_geometric"
+    ],
+    extras_require={
+
+    },
+    description="""
+        A Fair Benchmark for GNN-based Recommendation.
+    """,
+    license="GNU General Public License v3.0 (See LICENSE)",
+    long_description=open("README.rst", "r", encoding="utf-8").read(),
+    url="https://github.com/maenzhier/GRecX"
+)
