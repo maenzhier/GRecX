f690074f91dba6abded162b8ddffc782d0104335
hujunxianligong <hujunxianligong@gmail.com>
2021-11-24 22:25:57 +0800

    update NGCF

--- grecx/models/ngcf.py
+++ grecx/models/ngcf.py
@@ -1,57 +1,28 @@
 # coding=utf-8
 
-import tf_geometric as tfg
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Dropout
-from tf_geometric import SparseAdj
 import tf_sparse as tfs
-from tf_sparse import sparse_diag_matmul, diag_sparse_matmul
-from tf_geometric.utils.graph_utils import convert_edge_to_directed, add_self_loop_edge
 from grecx.models.light_gcn import LightGCN
 
 
-class NGCF(tf.keras.Model):
-
-    def __init__(self, units, k=1, activations=tf.nn.leaky_relu, drop_rates=0., *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.units = units
-        self.drop_rates = drop_rates
-
-        self.units_list = units if hasattr(units, "__len__") else [units] * k
-        self.k = len(self.units_list)
-        self.activation_list = activations if hasattr(activations, "__len__") else [activations] * self.k
-        self.drop_rate_list = drop_rates if hasattr(units, "__len__") else [drop_rates] * self.k
-
-        assert len(self.units_list) == len(self.drop_rate_list) == len(self.activation_list) == self.k
-
-        self.ngcf_layers = [NGCFConv(unit, activation, drop_rate) for unit, activation, drop_rate in
-                            zip(self.units_list, self.activation_list, self.drop_rate_list)]
-
-    def call(self, inputs, training=None, mask=None, cache=None):
-        x, edge_index = inputs
-
-        h = x
-        h_list = [h]
-
-        for i in range(self.k):
-
-            h = self.ngcf_layers[i]([edge_index, h], training=training, cache=cache)
-            h_list.append(tf.nn.l2_normalize(h, axis=-1))
-
-        h = tf.concat(h_list, axis=-1)
-
-        return h
-
-
 class NGCFConv(tf.keras.Model):
-    def __init__(self, units, activation=tf.nn.leaky_relu, drop_rate=0., *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.units = units
-        self.drop_rate = drop_rate
+    """
+    Each NGCF Convolutional Layer
+    """
 
-        self.dense1 = Dense(units, activation)
-        self.dense2 = Dense(units, activation)
-        self.dropout_layer = Dropout(drop_rate)
+    def __init__(self, dense_activation=tf.nn.leaky_relu, edge_drop_rate=0.0, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.dense_activation = dense_activation
+        self.gcn_dense = None
+        self.interaction_dense = None
+        self.edge_drop_rate = edge_drop_rate
+
+    def build(self, input_shape):
+        x_shape, _ = input_shape
+
+        self.gcn_dense = tf.keras.layers.Dense(x_shape[1], activation=self.dense_activation)
+        self.interaction_dense = tf.keras.layers.Dense(x_shape[1], activation=self.dense_activation)
 
     def build_cache_for_graph(self, graph, override=False):
         """
@@ -69,11 +40,54 @@ class NGCFConv(tf.keras.Model):
         x, edge_index = inputs[0], inputs[1]
 
         num_nodes = tfs.shape(x)[0]
-        normed_adj = LightGCN.norm_adj(edge_index, num_nodes=num_nodes, cache=cache)
+        normed_adj = LightGCN.norm_adj(edge_index, num_nodes=num_nodes, cache=cache)\
+            .dropout(self.edge_drop_rate, training=training)
 
         h = normed_adj @ x
-        h = self.dense1(h) + self.dense2(x * h)
-        
-        h = self.dropout_layer(h, training=training)
+        h = self.gcn_dense(h) + self.interaction_dense(x * h)
+
+        h = tf.nn.l2_normalize(h, axis=-1)
+
+        return h
+
+
+
+class NGCF(tf.keras.Model):
+
+    def __init__(self, units, k=3, activation=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.units = units
+        self.ngcf_convs = [NGCFConv() for _ in range(k)]
+
+        for i, ngcf_conv in enumerate(self.ngcf_convs):
+            setattr(self, "ngcf_conv{}".format(i), ngcf_conv)
+
+        self.activation = activation
+
+    def build_cache_for_graph(self, graph, override=False):
+        """
+        Manually compute the normed edge based on this layer's GCN normalization configuration (self.renorm and self.improved) and put it in graph.cache.
+        If the normed edge already exists in graph.cache and the override parameter is False, this method will do nothing.
+        :param graph: tfg.Graph, the input graph.
+        :param override: Whether to override existing cached normed edge.
+        :return: None
+        """
+        self.ngcf_convs[0].norm_adj(graph, override=override)
+
+    def call(self, inputs, training=None, mask=None, cache=None):
+        x, edge_index = inputs
+
+        h = x
+        h_list = [h]
+
+        for ngcf_conv in self.ngcf_convs:
+            h = ngcf_conv([edge_index, h], training=training, cache=cache)
+            h_list.append(h)
+
+        h = tf.concat(h_list, axis=-1)
+
+        if self.activation is not None:
+            h = self.activation(h)
 
         return h
