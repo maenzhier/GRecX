659b2e9488ca09bca2016a899b7814c1b2c2fcd6
maenzhier <457238187@qq.com>
2021-12-24 17:23:26 +0800

    update pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+import setuptools
+import io
 from setuptools import setup, find_packages
 
 setup(
     name="grecx",
     python_requires='>3.5.0',
-    version="0.0.3",
+    version="0.0.4.9",
     author="Desheng Cai",
     author_email="caidsml@gmail.com",
     packages=find_packages(
@@ -22,10 +24,10 @@ setup(
     install_requires=[
         "tf_geometric >= 0.0.77",
         "numpy >= 1.17.4",
-        "tensorflow == 2.4.1",
+        # "tensorflow == 2.4.1",
         "scikit-learn >= 0.22",
         "tqdm",
-        "Sphinx == 3.5.4",
+        # "Sphinx == 3.5.4",
         "faiss-cpu"
     ],
     extras_require={
@@ -34,6 +36,8 @@ setup(
     package_data={'grecx': ["metrics/libranking.so", "metrics/libranking.dll"]},
     description="An Efficient and Unified Benchmark for GNN-based Recommendation.",
     license="GNU General Public License v3.0 (See LICENSE)",
-    long_description=open("README.rst", "r", encoding="utf-8").read(),
+    long_description=io.open("README.rst", mode="r", encoding="utf-8").read(),
+    # long_description="An Efficient and Unified Benchmark for GNN-based Recommendation.",
+    long_description_content_type='text/x-rst',
     url="https://github.com/maenzhier/GRecX"
 )
